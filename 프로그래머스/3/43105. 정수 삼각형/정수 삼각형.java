d61dfd3ad07b5fca1ea815fea6d692886b8cef77
SEUNGEON KIM <tmdtmdqorekf@naver.com>
2024-08-12 19:14:23 +0900

    [level 3] Title: 정수 삼각형, Time: 13.82 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                if (j == 0) { // 제일 왼쪽 줄
+                    triangle[i][j] += triangle[i-1][j];
+                }
+                else if (i == j) { // 제일 오른쪽 줄
+                    triangle[i][j] += triangle[i-1][j-1];
+                } else { // 중간 줄들 계산
+                    triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
+                }
+                
+                answer = Math.max(answer, triangle[i][j]);
+            }
+        }
+    
+        return answer;
+    }
+}
