b9915edbb78d522de2fb7e5e41ba93bfd422303e
SEUNGEON KIM <tmdtmdqorekf@naver.com>
2024-08-12 17:35:02 +0900

    [level 2] Title: 타겟 넘버, Time: 5.16 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    int answer = 0;
+    
+    public int solution(int[] numbers, int target) {        
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+    
+    private void dfs(int[] numbers, int target, int depth, int sum) {
+        if (depth == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+        } else {
+            dfs(numbers, target, depth + 1, sum + numbers[depth]);
+            dfs(numbers, target, depth + 1, sum - numbers[depth]);
+        }
+    }
+}
