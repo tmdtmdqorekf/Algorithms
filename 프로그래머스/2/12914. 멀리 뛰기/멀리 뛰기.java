bed99742814eb3623af1804512dc00b8c6717489
SEUNGEON KIM <tmdtmdqorekf@naver.com>
2024-08-12 18:34:02 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        
+        long[] dp = new long[2001];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i < 2001; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
