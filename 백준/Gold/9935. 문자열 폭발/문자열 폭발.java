581b88d494b966810be36965117da877e0d10906
SEUNGEON KIM <tmdtmdqorekf@naver.com>
2024-09-30 11:36:02 +0900

    [Gold IV] Title: 문자열 폭발, Time: 464 ms, Memory: 33864 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,54 +1,48 @@
 import java.io.*;
-import java.time.chrono.IsoEra;
 import java.util.*;
 
 public class Main {
-    StringTokenizer st;
+    Stack<Character> s = new Stack<>();
 
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
-        String strExp = br.readLine();
+        String exp = br.readLine();
 
-        Stack<Character> s = new Stack<>();
-
-        // 1. 문자열을 하나씩 스택에 넣는다.
         for (int i = 0; i < str.length(); i++) {
             s.push(str.charAt(i));
 
-            // 2. 스택의 크기가 폭발 문자열과 크거나 같으면 비교한다.
-            if (s.size() >= strExp.length()) {
+            // 쌓인 문자열이 폭발 문자열보다 큰 경우에만 체크
+            if (s.size() >= exp.length()) {
                 boolean isExp = true;
 
-                for (int j = 0; j < strExp.length(); j++) {
-                    if (s.get(s.size() - strExp.length() + j) != strExp.charAt(j)) {
+                for (int j = 0; j < exp.length(); j++) {
+                    if (s.get(s.size() - exp.length() + j) != exp.charAt(j)) {
                         isExp = false;
                         break;
                     }
                 }
 
-                // 3. 폭발 문자열과 같으면 스택에서 제거한다.
                 if (isExp) {
-                    for (int j = 0; j < strExp.length(); j++) {
+                    for (int j = 0; j < exp.length(); j++) {
                         s.pop();
                     }
                 }
             }
         }
-        
+
         StringBuilder sb = new StringBuilder();
-        if (!s.isEmpty()) {
-            for (int i = 0; i < s.size(); i++) {
-                sb.append(s.get(i));
+        if (s.isEmpty()) {
+            System.out.print("FRULA");
+            return;
+        } else {
+            for (char x : s) {
+                sb.append(x);
             }
         }
 
-        if (sb.length() == 0) {
-            System.out.println("FRULA");
-        } else {
-            System.out.println(sb);
-        }
+        System.out.println(sb);
     }
 
     public static void main(String[] args) throws Exception {
