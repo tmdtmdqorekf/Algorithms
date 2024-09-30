e200903a9086d64d4ba893ea9dbd495b3da76cc5
SEUNGEON KIM <tmdtmdqorekf@naver.com>
2024-09-30 16:56:39 +0900

    [Silver II] Title: 사탕 게임, Time: 144 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,81 +1,71 @@
 import java.io.*;
-import java.util.*;
-
-class Candy {
-    int x, y;
-
-    public Candy(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
 
 public class Main {
-    int N, max;
+    int N, candy;
+    int maxCandy = Integer.MIN_VALUE;
     char[][] map;
-
-    // 상우하좌
-    int[] dx = {1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
 
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        // 초기화
         map = new char[N][N];
         for (int i = 0; i < N; i++) {
             map[i] = br.readLine().toCharArray();
         }
 
-        // 좌우 swap + 백트래킹
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N - 1; j++) {
+                // 좌우 swap
                 swap(i, j, i, j + 1);
                 calculate();
-                swap(i, j + 1, i, j);
+                swap(i, j, i, j + 1);
             }
         }
 
-        // 상하 swap + 백트래킹
         for (int i = 0; i < N - 1; i++) {
             for (int j = 0; j < N; j++) {
+                // 상하 swap
                 swap(i, j, i + 1, j);
                 calculate();
-                swap(i + 1, j, i, j);
+                swap(i, j, i + 1, j);
             }
         }
 
-        System.out.println(max);
+        System.out.println(maxCandy);
     }
 
+    // swap & back
     private void swap(int i1, int j1, int i2, int j2) {
         char tmp = map[i1][j1];
         map[i1][j1] = map[i2][j2];
         map[i2][j2] = tmp;
     }
 
+    // 먹을 수 있는 사탕 수 계산
     private void calculate() {
+        // 좌우 연속된 사탕 수
         for (int i = 0; i < N; i++) {
-            int cnt = 1;
+            candy = 1;
             for (int j = 0; j < N - 1; j++) {
                 if (map[i][j] == map[i][j+1]) {
-                    cnt++;
-                    max = Math.max(cnt, max);
+                    candy++;
+                    maxCandy = Math.max(maxCandy, candy);
                 } else {
-                    cnt = 1;
+                    candy = 1;
                 }
             }
         }
 
+        // 상하 연속된 사탕 수
         for (int i = 0; i < N; i++) {
-            int cnt = 1;
+            candy = 1;
             for (int j = 0; j < N - 1; j++) {
                 if (map[j][i] == map[j+1][i]) {
-                    cnt++;
-                    max = Math.max(cnt, max);
+                    candy++;
+                    maxCandy = Math.max(maxCandy, candy);
                 } else {
-                    cnt = 1;
+                    candy = 1;
                 }
             }
         }
