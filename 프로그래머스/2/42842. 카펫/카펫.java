5b0966a848a743c0fa08b6ae8c00e2bdcaceb42b
SEUNGEON KIM <tmdtmdqorekf@naver.com>
2024-10-02 11:43:35 +0900

    [level 2] Title: 카펫, Time: 155.11 ms, Memory: 214 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Pair {
+    int x, y;
+    
+    public Pair(int x, int y) {
+        this.x = x; // 가로
+        this.y = y; // 세로
+    }
+}
+
+class Solution {
+    int yellowCnt = 0;
+    
+    Queue<Pair> q = new LinkedList<>();
+    int[] answer = new int[2];
+    int[][] map;
+    
+    public int[] solution(int brown, int yellow) {        
+        int colorCnt = brown + yellow; // 12
+        
+        for (int i = 3; i <= Math.sqrt(colorCnt); i++) {
+            if (colorCnt % i == 0) { // i가 12의 약수일 경우
+                int n = colorCnt / i;
+                q.add(new Pair(n, i));
+            }
+        }
+        
+        while (!q.isEmpty()) {
+            Pair square = q.poll(); // (4, 3)
+            int w = square.x;
+            int h = square.y;
+            
+            yellowCnt = 0;
+            map = new int[w][h];
+            for (int i = 1; i < w - 1; i++) {
+                for (int j = 1; j < h - 1; j++) {
+                    yellowCnt++;
+                }
+            }
+            
+            if (yellowCnt == yellow) {
+                answer[0] = w;
+                answer[1] = h;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
