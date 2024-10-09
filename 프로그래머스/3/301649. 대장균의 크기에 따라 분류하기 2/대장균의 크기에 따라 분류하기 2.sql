447469510207435b8bf30e73923a2b84be74c687
SEUNGEON KIM <tmdtmdqorekf@naver.com>
2024-10-09 18:08:23 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,8 @@
+SELECT D.ID, (CASE WHEN D.R >= 0.75 THEN 'CRITICAL'
+                    WHEN D.R >= 0.5 THEN 'HIGH'
+                    WHEN D.R >= 0.25 THEN 'MEDIUM'
+                    ELSE 'LOW' END) AS COLONY_NAME
+FROM (
+    SELECT ID, PERCENT_RANK() OVER (ORDER BY SIZE_OF_COLONY) AS R
+    FROM ECOLI_DATA) AS D
+ORDER BY D.ID;
